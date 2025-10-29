686f52175b847c0e3341dab2ac6ec1c493556aff
Michel Alves <michelalvs@gmail.com>
2025-10-29 00:09:25 -0300

    Created User POST Endpoint

---
+++ src/backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseCreateUserJson), StatusCodes.Status201Created)]
+        public IActionResult CreateUser(RequestCreateUserJson request)
+        {
+            return Created();
+        }
+
+    }
+}
---
+++ src/shared/MyRecipeBook.Communication/Responses/ResponseCreateUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseCreateUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
